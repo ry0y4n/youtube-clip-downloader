6863c119dd27d9b77f4328cb4a387d456a573b75
momosuke <rusk.ryoy4n@gmail.com>
2023-03-29 17:12:48 +0900

    feature: add tweet function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@
 const youtubedl = require('youtube-dl-exec');
 const fs = require('fs');
 const express = require('express');
-const { start } = require('repl');
-
-const ytdlpPath = require.resolve('youtube-dl-exec');
+const { TwitterApi } = require('twitter-api-v2');
 
 const app = express();
+
+const client = new TwitterApi({
+    appKey: 'kycgG7g7ScsZMAQU6pIL8CKAI',
+    appSecret: 'UQVTQrmQEIFBGahuVUTR3FjQgZPZkiT9Hfk7VqJxiPBM1qxiV0',
+    accessToken: '1437613087-yK7chyOK7r2MFOCybEQPutTWVLiBTyjk15mKmOd',
+    accessSecret: 'TPLAN3hN9dQ13PvfBC9giZ4NNu48vrJWzfdpV2tyDf7YP',
+});
 
 const getInfo = (url, flags) => {
     return youtubedl(url, { dumpSingleJson: true, ...flags });
@@ -42,11 +47,16 @@ app.get("/download", async (req, res) => {
     const duration = req.query.duration;
 
     await downloadVideo(url, startTime, duration);
-
-    const filenames = fs.readdirSync(`${__dirname}/../video`);
-    console.log(filenames)
-
     console.log('completed downloading');
+
+    const mediaIds = await client.v1.uploadMedia(`${__dirname}/../video/video-clip.mp4`);
+    await client.v1.tweet('api test', { media_ids: mediaIds });
+    console.log('completed tweeting');
+    
+    fs.unlink(`${__dirname}/../video/video-clip.mp4`, ((err) => {
+        if (err) throw err;
+        console.log('completed cleaning');
+    }));
 
     res.sendStatus(200);
 });
